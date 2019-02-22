6320821b0bc64c08a3e23873fa471c213e79c1f0
Nate Armstrong <natearmstrong2@gmail.com>
2019-02-22 13:28:59 -0700

    Giving up!

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
+import com.google.api.client.auth.oauth2.BearerToken
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.testing.Testing
+
 fun main(args: Array<String>) {
-    println("Hello, world!")
+    val credentials = Credential.Builder(BearerToken.authorizationHeaderAccessMethod()).build()
+    val testing = Testing.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory(), credentials)
 }
